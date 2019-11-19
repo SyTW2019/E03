f8f6e34c9c2a748d1bf08b9001a835de8a61b464
diego-algomas <alu0101029993@ull.edu.es>
2019-11-19 18:21:12 +0000

    Arreglado el Input

--- app/components/Input/index.js
+++ app/components/Input/index.js
@@ -4,7 +4,13 @@ export default class Input extends React.Component {
   render(){
     return(
       <div>
-        <input/>
+        <input 
+          text={this.props.text}
+          ref={this.props.ref}
+          type={this.props.type}
+          value={this.props.value}
+          onChange={this.props.onChange}
+          />
       </div>
     );
   }
