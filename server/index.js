939887bc0ee085596fdc29c880b0554cc541cb3f
diego-algomas <alu0101029993@ull.edu.es>
2019-11-19 21:08:48 +0000

    Introduce datos en la base de datos

--- server/index.js
+++ server/index.js
@@ -14,50 +14,80 @@ const ngrok =
 const { resolve } = require('path');
 const app = express();
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+//Comunicación con la base de datos
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/pagina')
 
-// In production we need to pass these values in instead of relying on webpack
-setup(app, {
-  outputPath: resolve(process.cwd(), 'build'),
-  publicPath: '/',
-});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  var usuarioSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    email: String,
+    password: String,
+  })
+  var user = mongoose.model('User', usuarioSchema);
 
-// get the intended host and port number, use localhost and port 3000 if not provided
-const customHost = argv.host || process.env.HOST;
-const host = customHost || null; // Let http.Server use its default IPv6/4 host
-const prettyHost = customHost || 'localhost';
 
-// use the gzipped bundle
-app.get('*.js', (req, res, next) => {
-  req.url = req.url + '.gz'; // eslint-disable-line
-  res.set('Content-Encoding', 'gzip');
-  next();
-});
 
-app.use(express.urlencoded())
-app.use(express.json())
-app.post('/usuario',(req,res,next)=>{
-  console.log(req.body) 
-  res.send('recibido')
 
-})
-// Start your app.
-app.listen(port, host, async err => {
-  if (err) {
-    return logger.error(err.message);
-  }
 
-  // Connect to ngrok in dev mode
-  if (ngrok) {
-    let url;
-    try {
-      url = await ngrok.connect(port);
-    } catch (e) {
-      return logger.error(e);
+  // If you need a backend, e.g. an API, add your custom backend-specific middleware here
+  // app.use('/api', myApi);
+
+  // In production we need to pass these values in instead of relying on webpack
+  setup(app, {
+    outputPath: resolve(process.cwd(), 'build'),
+    publicPath: '/',
+  });
+
+  // get the intended host and port number, use localhost and port 3000 if not provided
+  const customHost = argv.host || process.env.HOST;
+  const host = customHost || null; // Let http.Server use its default IPv6/4 host
+  const prettyHost = customHost || 'localhost';
+
+  // use the gzipped bundle
+  app.get('*.js', (req, res, next) => {
+    req.url = req.url + '.gz'; // eslint-disable-line
+    res.set('Content-Encoding', 'gzip');
+    next();
+  });
+
+  app.use(express.urlencoded({extended:true}))
+  app.use(express.json())
+  app.post('/usuario', (req, res, next) => {
+    console.log(req.body)
+    var nuevoUsuario = new user ({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+    nuevoUsuario.save(function(err){
+      if(err)throw err;
+      console.log(`Usuario ${nuevoUsuario.name} guardado con exito`)
+    })
+    res.send('recibido')
+
+  })
+  // Start your app.
+  app.listen(port, host, async err => {
+    if (err) {
+      return logger.error(err.message);
     }
-    logger.appStarted(port, prettyHost, url);
-  } else {
-    logger.appStarted(port, prettyHost);
-  }
-});
+
+    // Connect to ngrok in dev mode
+    if (ngrok) {
+      let url;
+      try {
+        url = await ngrok.connect(port);
+      } catch (e) {
+        return logger.error(e);
+      }
+      logger.appStarted(port, prettyHost, url);
+    } else {
+      logger.appStarted(port, prettyHost);
+    }
+  });
+})
