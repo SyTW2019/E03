3367e716a940c78e9c95c993ff674b5b987878f5
Carmen <alu0101045603@ull.edu.es>
2019-11-26 10:33:03 +0000

    SignUp como en material-ui

---
+++ app/components/myH2/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class H2 extends React.Component {
+  render(){
+    return(
+      <h2>{this.props.title}</h2>
+    );
+  }
+}
---
+++ app/components/myH3/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class H3 extends React.Component {
+  render(){
+    return(
+      <h3 style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>{this.props.title}</h3>
+    );
+  }
+}
