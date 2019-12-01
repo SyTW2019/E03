8a02721cc40797a6a812aa8b595afd5acb0f6d8f
Carmen <alu0101045603@ull.edu.es>
2019-12-01 01:01:31 +0000

    Grid añadido al Home

--- app/components/Card/index.js
+++ app/components/Card/index.js
@@ -6,9 +6,10 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Image from './book.jpg'
 
 const useStyles = makeStyles({
   card: {
@@ -23,33 +24,70 @@ export default function ImgMediaCard() {
   const classes = useStyles();
 
   return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-         image= {require('./book.jpg')}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Event
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-	  Esto es una tarjeta para un evento. Aqui se inserta la descripcion y detalles del Evento.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    <Container component="main" maxWidth="xs">
+      <div>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="140"
+                image= {require('./book.jpg')}
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Event
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+            Esto es una tarjeta para un evento. Aqui se inserta la descripcion y detalles del Evento.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="140"
+                image= {require('./concierto.jpg')}
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Event
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+            Esto es una tarjeta para un evento. Aqui se inserta la descripcion y detalles del Evento.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
   );
 }
 
