0845d0248bc4638290678030d64122358b77eecc
diego-algomas <alu0101029993@ull.edu.es>
2019-12-12 17:57:33 +0000

    cambio de intlprovider

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -24,35 +24,37 @@ import InicioSesion from '../InicioSesion';
 import Categoria from '../Categorias';
 import NotFound from '../NotFound';
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
-      <Router>
-      <div>
-        <Header/>
-        <Switch>
-          <Route exact path="/" component={Inicio}/>
-          <Route path="/SignUp" component={Registro}/>
-          <Route path="/SignIn" component={InicioSesion}/>
-          <Route path="/Category" component={Categoria}/>
-          <Route component={NotFound}/>
-        </Switch>
-        <Footer/>
-      </div>  
-      </Router>
-   /*   <IntlProvider locale="en">
+      <IntlProvider locale="en">
         <Router>
-          <Header/>
-          <Switch>
-            <Route exact path='/' Component={Card}><Card/></Route>
-            <Route path='/SignUp' Component={Registro}><Registro/></Route>
-            <Route path='/SignIn' Component={Inicio}><Inicio/></Route>
-            <Route path='/Category' Component={Categoria}><Categoria/></Route>
-            <Route path="*" component={NotFound}><NotFound/></Route>
-          </Switch>
-          <Footer/>
+          <div>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Inicio} />
+              <Route path="/SignUp" component={Registro} />
+              <Route path="/SignIn" component={InicioSesion} />
+              <Route path="/Category" component={Categoria} />
+              <Route component={NotFound} />
+            </Switch>
+            <Footer />
+          </div>
         </Router>
-      </IntlProvider>*/
+      </IntlProvider>
+      /*   <IntlProvider locale="en">
+           <Router>
+             <Header/>
+             <Switch>
+               <Route exact path='/' Component={Card}><Card/></Route>
+               <Route path='/SignUp' Component={Registro}><Registro/></Route>
+               <Route path='/SignIn' Component={Inicio}><Inicio/></Route>
+               <Route path='/Category' Component={Categoria}><Categoria/></Route>
+               <Route path="*" component={NotFound}><NotFound/></Route>
+             </Switch>
+             <Footer/>
+           </Router>
+         </IntlProvider>*/
 
     );
   }
