b9c6a59cd9a6d1954741911d3d29a982df37cb8f
alu0101061672 <alu0101061672@ull.edu.es>
2020-03-16 19:28:22 +0000

    header decente

--- frontend/src/_components/Header/HeaderLink.js
+++ frontend/src/_components/Header/HeaderLink.js
@@ -6,21 +6,13 @@ export default styled(Link)`
   padding: 0.25em 2em;
   margin: 1em;
   text-decoration: none;
-  border-radius: 4px;
-  -webkit-font-smoothing: antialiased;
-  -webkit-touch-callout: none;
   user-select: none;
   cursor: pointer;
-  outline: 0;
-  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  font-weight: bold;
-  font-size: 16px;
-  border-right: 4px solid #000;
-  border-bottom: 3px solid #000; 
-  color: #000;
+  font-size: 18px;
+  color: black;
 
   &:active {
-    background: #000;
-    color: #fff;
+    background: transparent;
+    color: transparent;
   }
 `;
