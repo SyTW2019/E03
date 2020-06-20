8de707309e21b134c6f0fc8a650ed0080754f39a
Gandares <oscar.glez.97@hotmail.com>
2020-06-20 18:19:45 +0100

    Tests corregidos

--- frontend/src/_components/Card/index.js
+++ frontend/src/_components/Card/index.js
@@ -7,8 +7,7 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import { Detail } from '../../Detail'
-import { assets } from '../../_helpers'
+import { assets } from '../../_helpers/assets'
 
 const useStyles = makeStyles({
   card: {
